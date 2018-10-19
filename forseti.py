c1c81c5e326d72a38e5ed940fad2ae594ec7f394
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-10-19 12:22:58 +0100

    Fix update bug

--- forseti.py
+++ forseti.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import sqlite3
 
+
 class Forseti:
     def __init__(self, queue):
         self.queue = queue
@@ -10,29 +11,23 @@ class Forseti:
         self.c.execute("PRAGMA journal_mode=WAL;")
         if self.c.fetchall()[0][0] != "wal":
             print("Error enabling write-ahead lookup!")
-        
+
     def main(self):
         while True:
             incoming = self.queue.get()
 
             query, values, mode = incoming[0], incoming[1], incoming[2]
-            
-            if mode == 'execute':
-                try:
+            try:
+                if mode == 'execute':
                     self.c.execute(query, values)
-                except:
-                    pass
-
-            elif mode == 'executemany':
-                try:
+                elif mode == 'executemany':
                     self.c.executemany(query, values)
-                except:
-                    pass
+            except:
+                pass
 
             self.conn.commit()
 
 
 def main(queue):
     forseti = Forseti(queue)
-    
     forseti.main()
