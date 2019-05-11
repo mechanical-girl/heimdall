1794aaf4be2c4ed765d04c91bc436953429e2e8c
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2019-05-11 15:04:03 +0100

    Revert "Add support for no roots.json"

--- roots.py
+++ roots.py
@@ -4,19 +4,13 @@ import os
 import subprocess
 import sys
 
-try:
-    with open("roots.json", "r") as f:
-        root_pid = json.loads(f.read())
-
-    result = subprocess.run(['ps', f"{root_pid}"], stdout=subprocess.PIPE)
-    pid = result.stdout.decode('utf-8').split('\n')[1]
-    if pid != '':
-        sys.exit(0)
-
-except FileNotFoundError:
-    with open("roots.json", "w") as f:
-        f.write('')
-
+with open("roots.json", "r") as f:
+    root_pid = json.loads(f.read())
+result = subprocess.run(['ps', f"{root_pid}"], stdout=subprocess.PIPE)
+pid = result.stdout.decode('utf-8').split('\n')[1]#.split()[0]
+if pid != '':
+    pid = pid.split()[0]
+    sys.exit(0)
 with open(os.devnull, 'w') as f:
     proc = subprocess.Popen(['pipenv', 'run', 'python', 'yggdrasil.py'])
 with open("roots.json", "w") as f:
