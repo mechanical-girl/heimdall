1eea6cd27be233c6f8d7f2c3e07c3d6d119abd05
PouncySilverkitten <struan@duncan-wilson.co.uk>
2019-07-10 08:45:34 +0100

    Add support for fill_in

--- yggdrasil.py
+++ yggdrasil.py
@@ -31,6 +31,8 @@ class Yggdrasil:
         parser.add_argument("-v", "--verbose", action="store_true", dest="verbose")
         parser.add_argument("--force-new-logs", help="If enabled, Heimdall will delete any current logs for the room", action="store_true", dest="new_logs")
         parser.add_argument("--use-logs", type=str, dest="use_logs")
+        parser.add_argument("--fill-in", "-f", action="store_true", dest="fill_in")
+
         args = parser.parse_args()
 
         self.room = args.room
@@ -38,6 +40,7 @@ class Yggdrasil:
         self.new_logs = args.new_logs
         self.use_logs = args.use_logs
         self.verbose = args.verbose
+        self.fill_in = args.fill_in
 
         with open('rooms.json') as f:
             self.rooms = json.loads(f.read())
@@ -64,7 +67,7 @@ class Yggdrasil:
 
         for room in self.rooms:
             try:
-                instance = mp.Process(target=self.run_heimdall, args=(room, self.stealth, self.new_logs, self.use_logs, self.verbose, self.queue))
+                instance = mp.Process(target=self.run_heimdall, args=(room, self.stealth, self.new_logs, self.use_logs, self.verbose, self.fill_in, self.queue))
                 instance.daemon = True
                 instance.name = room
                 self.instances.append(instance)
@@ -88,12 +91,12 @@ class Yggdrasil:
             self.logger.exception(f"Error initialising forseti")
 
 
-    def run_heimdall(self, room, stealth, new_logs, use_logs, verbose, queue):
+    def run_heimdall(self, room, stealth, new_logs, use_logs, verbose, fill_in, queue):
         try:
             if room == "test":
-                heimdall.main((room, queue), stealth=stealth, new_logs=new_logs, use_logs="xkcd", verbose=verbose)
+                heimdall.main((room, queue), stealth=stealth, new_logs=new_logs, use_logs="xkcd", verbose=verbose, fill_in)
             else:
-                heimdall.main((room, queue), stealth=stealth, new_logs=new_logs, use_logs=use_logs, verbose=verbose)
+                heimdall.main((room, queue), stealth=stealth, new_logs=new_logs, use_logs=use_logs, verbose=verbose, fill_in)
         except:
             self.logger.exception(f"Error initialising heimdall in {room}")
 
